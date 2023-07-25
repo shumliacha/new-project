645bbc13329a3cfcea3b231edbd13c54426997c5
shumliacha <126417371+shumliacha@users.noreply.github.com>
2023-07-25 20:05:08 +0200

    Initial Commit

---
+++ Moodly/MoodlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoodlyApp.swift
+//  Moodly
+//
+//  Created by Kristina Litvinova on 25.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct MoodlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
